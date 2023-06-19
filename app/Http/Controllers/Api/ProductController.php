b33691bd269be926bf68fbcaec403b02bd58c2e6
DavideSberna <sbernadavide@gmail.com>
2023-06-19 15:49:13 +0200

    complete branch

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,15 +5,29 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with('brand', 'category')->paginate(5);
+
+        if (!empty($request->query('category_id'))) {
+            $category_id = $request->query('category_id');
+            $products = Product::where('category_id', $category_id)->with('brand', 'category')->paginate(5);
+        } else {
+            $products = Product::with('brand', 'category')->paginate(5);
+        }
+
+        $categories = Category::all();
+
         return response()->json([
             'success' => true,
-            'results' => $products
+            'results' => [
+                'products' => $products,
+                'categories' => $categories,
+
+            ]
         ]);
     }
 
